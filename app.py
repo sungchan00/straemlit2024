1756fb74881ab5826e3444daf72bc8ccddb43c3e
sungchan00 <t2024095@sasang.hs.kr>
2024-07-24 11:40:24 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
 import streamlit as st
+import random 
 
-st.write('hello Streamlit!!:sungchan:')
+def show() :
+  st.header('주사위 게임', divider='rainbow')
+  clicked = st.button('주사위던지기', type='primary')
+  if clicked :
+    n = random.randint(1, 6)
+    st.image(f'./img/{n}.png')
+
+
+    
